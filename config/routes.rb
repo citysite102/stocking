baa74889de999398a3a20be17f01ebc2589561b5
Samuel <citysite1025@gmail.com>
2019-07-14 16:54:54 +0800

    Support for bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # get 'home/index'
   root 'home#index'# 定義 Homepage
+  get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
