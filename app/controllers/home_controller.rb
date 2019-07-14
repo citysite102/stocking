c0bc1fda9f5d0c2933c79cb8571cbcfb5843445a
Samuel <citysite1025@gmail.com>
2019-07-15 00:02:26 +0800

    Update devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+  	if params[:ticker] == ""
+  		@nothing = "Hey, You Forgot something"
+  	elsif params[:ticker]
+  		begin 
+	  		@stock = StockQuote::Stock.quote(params[:ticker])
+	  	rescue StandardError
+	  		@error = "Hey! That Stock Stock Doesn't Exist!"
+	  	end
+  	end
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'home/index'
   root 'home#index'# 定義 Homepage
   get 'home/about'
+
+  post "/" => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
