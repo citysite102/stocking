dbef11aa00ab9cc4d9a782cf90b2a148f6f697d1
Samuel <citysite1025@gmail.com>
2019-07-14 15:48:17 +0800

    Initial conmmit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # get 'home/index'
+  root 'home#index'# 定義 Homepage
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
